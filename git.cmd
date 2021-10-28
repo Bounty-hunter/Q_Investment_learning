36ea79a3019f7818ebee1333ca66a7d50019a69f
面向图计算的通用计算机技术与系统 <tujisuan@163.com>
2021-10-28 20:48:32 +0800

    21-10-28

---
+++ git.cmd
@@ -0,0 +1,7 @@
+git status #查看状态
+git ls-files #查看包含的文件
+git add XXX #添加到本地git
+git commit -m "message" # 提交该版本
+git push origin main #更新到远端仓库，origin 远端名，main 分支名
+git rm XXX #从仓库中移除文件，同时删除本地文件
+git rm --cached XXX #从仓库中移除文件，保留本地文件
